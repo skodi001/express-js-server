a08cd26d43c1bffc5e9f9d6d511a19077ca3dc3c
Shokhmurod <shokhpjwstk@gmail.com>
2023-01-06 21:50:52 +0100

    understanding routes with express

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Contact me");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About me");
+});
+
+app.get("/hobbie", (req, res) => {
+  res.send("football");
+});
+
 app.listen(3000, function () {
   console.log("Server listening on port 3000");
 });
